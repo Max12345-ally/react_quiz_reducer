15105c6d76c324ca7474cf8bea0394827e894bff
maxbond <ceo@rf.ru>
2024-06-08 00:09:59 +0300

    ready to start project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import CreateCustomer from "./CreateCustomer";
-import Customer from "./Customer";
-import AccountOperations from "./AccountOperations";
-import BalanceDisplay from "./BalanceDisplay";
+import DateCounter from './DateCounter';
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      <DateCounter />
     </div>
   );
 }
-
-export default App;
