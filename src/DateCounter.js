f839f8a23cdeb5fe16b304794f69c47fac961331
maxbond <ceo@rf.ru>
2024-06-13 23:54:46 +0300

    useReducer in progress

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -9,9 +9,11 @@ function reducer(state, action) {
 function DateCounter() {
   // const [count, setCount] = useState(0);
 
-  const [count, dispatch] = useReducer(reducer, 0);
+  const initialState = {count: 0, step: 1};
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {count, step} = state;
 
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
@@ -35,12 +37,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
