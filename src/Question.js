cc34b98513e6d223d862a5a84b850960a909c7bb
maxbond <ceo@rf.ru>
2024-07-19 16:59:03 +0300

    Question component filled

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function Question() {
-  return <div>Question</div>;
+function Question({question}) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <div className='options'>
+        {question.options.map((option) => (
+          <button className='btn btn-option'>{option}</button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Question;
