6d3ad4cbb5eb3b231ab6085f3c71cf4903865297
maxbond <ceo@rf.ru>
2024-07-24 21:34:43 +0300

    receiving answers in component

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   status: 'loading',
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -27,6 +28,8 @@ function reducer(state, action) {
       };
     case 'start':
       return {...state, status: 'active'};
+    case 'newAnswer':
+      return {...state, answer: action.payload};
 
     default:
       throw new Error('Action unknown');
@@ -34,7 +37,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{questions, status, index}, dispatch] = useReducer(
+  const [{questions, status, index, answer}, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -57,7 +60,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
         {}
       </Main>
     </div>
